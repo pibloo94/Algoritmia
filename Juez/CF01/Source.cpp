f943b3e834f5193e296285a1f9c2a367b602f8b4
Pablo Agudo Brun <32678514+pibloo94@users.noreply.github.com>
2018-10-23 16:02:00 +0200

    Update Source.cpp

--- Juez/CF01/Source.cpp
+++ Juez/CF01/Source.cpp
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-//{P: true}
+
+/*
+{P: 0 < v.Length <= 1000 && forall k:: 0 <= k < v.Length ==> -50 <= v[k] <= 60}
+fun picos(v: array<int>) returns(contPicos: int)
+{Q: contPicos = #u: 0 <= z < v.Length-1: v[u-1] < v[u] > v[u+1)}
+*/ 
 int picos(vector<int>& v) {
 	int contPicos = 0;
 	//{I: 1 <= i < v.size()}
@@ -18,9 +23,12 @@ int picos(vector<int>& v) {
 	}
 	return contPicos;
 }
-//{Q: contPicos = #u: forall k: 1 <= k < n-1: a[k] > a[k+1] && a[k] > a[k-1]}
 
-//{P: true}
+/*
+{P: 0 < v.Length <= 1000 && forall k:: 0 <= k < v.Length ==> -50 <= v[k] <= 60}
+fun valles(v: array<int>) returns(contValles: int)
+{Q: contValles = #u: 0 <= z < v.Length-1: v[u-1] > v[u] < v[u+1)}
+*/ 
 int valles(vector<int>& v) {
 	int contValles = 0;
 	//{I: 1 <= i < v.size()}
@@ -32,8 +40,10 @@ int valles(vector<int>& v) {
 	}
 	return contValles;
 }
-//{Q: contValles = #u: forall k: 1 <= k < n-1: a[k] < a[k+1] && a[k] < a[k-1]}
 
+/*
+void resuelveCaso(int v[], int n, int& nPicos, int& nValles)
+*/
 void resuelveCaso() {
 
 	std::vector<int> v;
@@ -52,13 +62,6 @@ void resuelveCaso() {
 int main() {
 	// Para la entrada por fichero.
 
-	/*
-	#ifndef DOMJUDGE
-		std::ifstream in("casos.txt");
-		auto cinbuf = std::cin.rdbuf(in.rdbuf());
-	#endif
-	*/
-
 	unsigned int numCasos;
 	std::cin >> numCasos;
 
@@ -66,13 +69,6 @@ int main() {
 	for (int i = 0; i < numCasos; ++i) {
 		resuelveCaso();
 	}
-
-	/*
-	#ifndef DOMJUDGE // para dejar todo como estaba al principio
-	std::cin.rdbuf(cinbuf);
-	system("PAUSE");
-	#endif
-	*/
-
+	
 	return 0;
 }
