bfdec156c091073b50c92425b0990244d6be34d2
Pablo Agudo Brun <32678514+pibloo94@users.noreply.github.com>
2018-11-07 09:43:51 +0100

    Add files via upload

--- Juez/CF13/Source.cpp
+++ Juez/CF13/Source.cpp
@@ -6,28 +6,26 @@
 using namespace std;
 
 struct sol {
-  long int numero;
-  long int pot = 0;
+  long long int numero = 0;
+  long long int pot = 1;
 };
 
+//{P: 0 <= n <= (2^31)-1}
 sol invertir(long int n) {
 
-  sol solucion, aux;
+  sol solucion;
 
-  if (n == 0) {
-    solucion.numero = 0;
-    solucion.pot = 1;
-    return solucion;
+  if (n != 0) {
+	solucion = invertir(n / 10);
+	solucion.numero += (n % 10 * solucion.pot);
+	solucion.pot = solucion.pot * 10;
   }
-  else {
-    aux = invertir(n / 10);
-    solucion.numero = aux.numero + (n % 10 * aux.pot);
-    solucion.pot = aux.pot * 10;
-    return solucion;
-  }
+
+  return solucion;
 }
+//{Q: solucion = invertir(n/10)}
 
-void resuelveCaso(long int n) {
+void resuelveCaso(long long int n) {
   //resuelve aqui tu caso
      //Lee los datos
      //Calcula el resultado
@@ -41,21 +39,23 @@ void resuelveCaso(long int n) {
 
 int main() {
   // Para la entrada por fichero.
+
+  /*
 #ifndef DOMJUDGE
   std::ifstream in("casos.txt");
   auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
-
-  unsigned long int n;
+  */
+  long long int n;
   // Resolvemos
   while (std::cin >> n) {
     resuelveCaso(n);
   }
-
+  /*
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
   std::cin.rdbuf(cinbuf);
   system("PAUSE");
 #endif
-
+  */
   return 0;
 }
