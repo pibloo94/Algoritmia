10d9d166ced6f668a0df74064282635b619aaa7e
Pablo Agudo Brun <32678514+pibloo94@users.noreply.github.com>
2019-01-07 18:26:46 +0100

    Add files via upload

---
+++ Juez/CF27/Source0.cpp
@@ -0,0 +1,110 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+//funcion que devuelve un vector de acumulacion de los tiempos minimos por cada funcionario
+std::vector<int> acumulados(std::vector<std::vector<int>> const& v, int& n) {
+	std::vector<int> minimos(n);
+	int minFila;
+
+	for (int i = 0; i < v.size(); i++) {
+		minFila = v[0][i];
+		for (int j = 1; j < n; j++) {
+			if (v[j][i] < minFila) {
+				minFila = v[j][i];
+			}
+		}
+		minimos[i] = minFila;
+	}
+
+	//acumulos los tiempos minimos de cada funcionario
+	std::vector<int> estimacion (n);
+	estimacion[n - 1] = minimos[n - 1];
+
+	for (int i = v.size() - 2; i >= 0; i--) {
+		estimacion[i] = estimacion[i + 1] + minimos[i];
+	}
+
+	return estimacion;
+}
+
+void resolver(std::vector<std::vector<int>> const& v, 
+	std::vector<bool>& marcaje,
+	std::vector<int>& acumulados,
+	int k, int& n, int& sumMin, int& sum) {
+
+	for (int i = 0; i < n; i++) {
+
+		if (!marcaje[i]) {
+			sum += v[i][k];
+			marcaje[i] = true; // marcaje
+
+			if (k == n - 1) {
+				if (sum < sumMin) {
+					sumMin = sum;
+				}
+			}
+			else {
+				if (sum + acumulados[k + 1] < sumMin) {
+				resolver(v, marcaje, acumulados, k + 1, n, sumMin, sum);
+				}
+			}
+			marcaje[i] = false; // desmarcaje
+			sum -= v[i][k];
+		}
+	}
+}
+
+bool resuelveCaso() {
+
+	int n, sum = 0, sumMin;
+	
+	std::cin >> n; // numero de funcionarios y trabajos
+
+	if (n == 0) {
+		return false;
+	}
+
+	std::vector<std::vector<int>> v(n, std::vector<int>(n)); //matriz de tiempos
+	std::vector<bool> marcaje (n, false); //vector marcaje inicializada a false
+	std::vector<int> acumulado;
+
+	//relleno la matriz
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			std::cin >> v[i][j];
+		}
+	}
+
+	acumulado = acumulados(v, n);
+	sumMin = INT_MAX;
+
+	resolver(v, marcaje, acumulado, 0, n, sumMin, sum);
+
+	std::cout << sumMin << endl;
+
+	return true;
+}
+
+int main() {
+	// Para la entrada por fichero.
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	// Resolvemos
+	while (resuelveCaso());
+
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Juez/CF27/Source2.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+
+void resolver(std::vector<std::vector<int>> const& v,
+	std::vector<bool>& marcaje,
+	int k, int const& n, int& sumMin, int& sum, int const& constante) {
+
+	for (int i = 0; i < n; i++) {
+
+		if (!marcaje[i]) {
+			sum += v[i][k];
+			marcaje[i] = true; // marcaje
+
+			if (k == n - 1) {
+				if (sum < sumMin) {
+					sumMin = sum;
+				}
+			}
+			else {
+				if (sum + (n - 1 - k)*constante < sumMin) { //si puede haber una suma minima mejor
+					resolver(v, marcaje, k + 1, n, sumMin, sum, constante);
+				}
+			}
+			marcaje[i] = false; // desmarcaje
+			sum -= v[i][k];
+		}
+	}
+}
+
+bool resuelveCaso() {
+
+	int n, sum = 0, sumMin;
+	int constante = INT_MAX;
+
+	std::cin >> n; // numero de funcionarios y trabajos
+
+	if (n == 0) {
+		return false;
+	}
+
+	std::vector<std::vector<int>> v(n, std::vector<int>(n)); //matriz de tiempos
+	std::vector<bool> marcaje(n, false); //vector marcaje inicializada a false
+
+	//relleno la matriz
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			std::cin >> v[i][j];
+			if (v[i][j] < constante) {
+				constante = v[i][j];
+			}
+		}
+	}
+
+	sumMin = INT_MAX;
+
+	resolver(v, marcaje, 0, n, sumMin, sum, constante);
+
+	std::cout << sumMin << endl;
+
+	return true;
+}
+
+int main() {
+	// Para la entrada por fichero.
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	// Resolvemos
+	while (resuelveCaso());
+
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
